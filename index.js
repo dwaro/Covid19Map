2d7fd9bde65d463e6adf127149ab6d90e72b41b7
Jacob Waro <davidjwaro@gmail.com>
2020-03-20 00:31:46 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets like main.js or main.css files
+  app.use(express.static("client/build"));
+
+  // Express will serve up index.html file if it does not recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("App is running on port: " + PORT);
+});
